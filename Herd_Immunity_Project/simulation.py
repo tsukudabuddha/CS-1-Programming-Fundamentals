4aee5be36ad3cc5e25e6a2b45691d8f79c7df724
tsukudabuddha <andrewtsukuda@gmail.com>
2017-10-20 16:35:57 -0700

    fixed some linter errors

--- Herd_Immunity_Project/simulation.py
+++ Herd_Immunity_Project/simulation.py
@@ -9,28 +9,31 @@ r.seed(42)
 
 class Simulation(object):
     '''
-    Main class that will run the herd immunity simulation program.  Expects initialization
-    parameters passed as command line arguments when file is run.
+    Main class that will run the herd immunity simulation program.
+    Expects initialization parameters passed as command line arguments
+    when file is run.
 
-    Simulates the spread of a virus through a given population.  The percentage of the
-    population that are vaccinated, the size of the population, and the amount of initially
-    infected people in a population are all variables that can be set when the program is run.
+    Simulates the spread of a virus through a given population.
+    The percentage of the population that are vaccinated, the size of the
+    population, and the amount of initially infected people in a population
+    are all variables that can be set when the program is run.
 
     _____Attributes______
 
-    logger: Logger object.  The helper object that will be responsible for writing
-    all logs to the simulation.
+    logger: Logger object.  The helper object that will be responsible for
+    writing all logs to the simulation.
 
     population_size: Int.  The size of the population for this simulation.
 
     population: [Person].  A list of person objects representing all people in
         the population.
 
-    next_person_id: Int.  The next available id value for all created person objects.
+    next_person_id: Int.  The next available id value for all created person
+    objects.
         Each person should have a unique _id value.
 
-    virus_name: String.  The name of the virus for the simulation.  This will be passed
-    to the Virus object upon instantiation.
+    virus_name: String.  The name of the virus for the simulation.  This will
+    be passed to the Virus object upon instantiation.
 
     mortality_rate: Float between 0 and 1.  This will be passed
     to the Virus object upon instantiation.
@@ -38,39 +41,44 @@ class Simulation(object):
     basic_repro_num: Float between 0 and 1.   This will be passed
     to the Virus object upon instantiation.
 
-    vacc_percentage: Float between 0 and 1.  Represents the total percentage of population
-        vaccinated for the given simulation.
+    vacc_percentage: Float between 0 and 1.  Represents the total percentage
+    of population vaccinated for the given simulation.
 
-    current_infected: Int.  The number of currently people in the population currently
-        infected with the disease in the simulation.
+    current_infected: Int.  The number of currently people in the population
+    currently infected with the disease in the simulation.
 
-    total_infected: Int.  The running total of people that have been infected since the
-    simulation began, including any people currently infected.
+    total_infected: Int.  The running total of people that have been infected
+    since the simulation began, including any people currently infected.
 
-    total_dead: Int.  The number of people that have died as a result of the infection
-        during this simulation.  Starts at zero.
+    total_dead: Int.  The number of people that have died as a result of the
+    infection during this simulation.  Starts at zero.
 
 
     _____Methods_____
 
     __init__(population_size, vacc_percentage, virus_name, mortality_rate,
      basic_repro_num, initial_infected=1):
-        -- All arguments will be passed as command-line arguments when the file is run.
-        -- After setting values for attributes, calls self._create_population() in order
-            to create the population array that will be used for this simulation.
+        -- All arguments will be passed as command-line arguments when the file
+            is run.
+        -- After setting values for attributes, calls self._create_population()
+            in order to create the population array that will be used for this
+            simulation.
 
     _create_population(self, initial_infected):
         -- Expects initial_infected as an Int.
         -- Should be called only once, at the end of the __init__ method.
-        -- Stores all newly created Person objects in a local variable, population.
-        -- Creates all infected person objects first.  Each time a new one is created,
-            increments infected_count variable by 1.
-        -- Once all infected person objects are created, begins creating healthy
-            person objects.  To decide if a person is vaccinated or not, generates
-            a random number between 0 and 1.  If that number is smaller than
-            self.vacc_percentage, new person object will be created with is_vaccinated
-            set to True.  Otherwise, is_vaccinated will be set to False.
-        -- Once len(population) is the same as self.population_size, returns population.
+            -- Stores all newly created Person objects in a local variable,
+        population.
+        -- Creates all infected person objects first.  Each time a new one is
+            created, increments infected_count variable by 1.
+        -- Once all infected person objects are created, begins creating
+            healthy person objects.  To decide if a person is vaccinated or
+            not, generates a random number between 0 and 1.  If that number is
+            smaller than self.vacc_percentage, new person object will be
+            created with is_vaccinated set to True.  Otherwise, is_vaccinated
+            will be set to False.
+        -- Once len(population) is the same as self.population_size,
+            returns population.
     '''
 
     def __init__(self, population_size, vacc_percentage, virus_name,
@@ -88,7 +96,7 @@ class Simulation(object):
             virus_name, population_size, vacc_percentage, initial_infected)
 
         self.logger = Logger(self.file_name)
-
+        # Create list to hold the newly infected peopel
         self.newly_infected = []
 
         # Create and save initial population
